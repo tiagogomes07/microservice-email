719417d4a2c958705bead3e8ab9c80ce1708abe2
tiagogomes07 <tiagogomes07@gmail.com>
2018-07-29 08:32:39 -0300

    Limpeza arquivos

--- frontmail/themed-context.js
+++
@@ -1,6 +0,0 @@
-// Make sure the shape of the default value passed to
-// createContext matches the shape that the consumers expect!
-export const ThemeContext = React.createContext({
-  theme: themes.dark,
-  toggleTheme: () => {}
-});
